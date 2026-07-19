d045ef566fa36c24dda207dbb6d651cfc3af3aa3
donebro100-heybro <playpointsbk@gmail.com>
2026-07-19 11:49:40 +0630

    Update run.py

--- run.py
+++ run.py
@@ -1,2 +1,27 @@
+import subprocess
 import e
-print(dir(e))
+
+try:
+    subprocess.run(
+        ["adb", "start-server"],
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL
+    )
+except:
+    pass
+
+e.check_activation()
+
+while True:
+    e.main_menu()
+    choice = input("\nSelect Option (1, 2 or 3 to Exit): ").strip()
+
+    if choice == "1":
+        e.option_adb_connect()
+    elif choice == "2":
+        e.option_auto_bypass()
+    elif choice == "3":
+        print("\n[+] Goodbye!")
+        break
+    else:
+        print("[-] Invalid Option!")
